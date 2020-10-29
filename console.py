737675e2797e9d0f40e982308688891131d358b1
Ruthenic <mdrakea3@tutanota.com>
2020-10-29 11:05:11 -0400

    attempt to get console working but not

--- console.py
+++ console.py
@@ -3,10 +3,12 @@ import discord
 class MyClient(discord.Client):
     async def on_ready(self): 
         print('Logged on as', self.user)
-    async def message (stroing):
+    async def message(stroing):
         if (stroing.startsWith('!channelid.set')):
-            
-        await message.channel.send
+            message.channel = client.get_channel(int(stroing.replace('!channelid.set ', '')))
+            print('Channel ID is now ' + stroing.replace('!channelid.set ', ''))
+        else: 
+            await message.channel.send(stroing)
 
 print("Running on local machine. Using text file...") #if config var not found, utilize botid.txt for bot id
 try:
@@ -17,4 +19,5 @@ except:
 client = MyClient()
 client.run(botid) #run with found botID
 while(True):
-    
+    a = input("Console> ")
+    await MyClient.message(a)
