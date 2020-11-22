429157ec7c91bd0e2c0e94590278a5c69f5b677f
Ruthenic <mdrakea3@tutanota.com>
2020-11-21 21:58:33 -0700

    Change credits

--- scripts/main.py
+++ scripts/main.py
@@ -16,7 +16,7 @@ from translate import Translator
 #firebase_admin.initialize_app(cred)
 #db = firestore.client()
 #servers_ref = db.collection(u"servers")
-credits = " People who've contributed: \nRuthenic (Drake),\ntestersbastarps (onboho),\nGnog3 (Gnog3)"
+credits = " People who've contributed: \nRuthenic (AD),\ntestersbastarps (onboho),\nGnog3 (Gnog3)"
 help_message = 'YaDiscord Bot\'s commands:\n`!/help` Show the command list.\n`!/credits` Basically credits.\n`!/ping` Ping the bot.\n`!/owo` Print a random OwO/UwU\n`!/say (text)` Make the bot say something.\n`!/range (first-number), (second-number)` Make the bot generate a random number in given range.\n`!/math (math-stuff)` Do simple math\n`!/eval` Evalutate something. Owner only.' #very long string, i know. do i care? no
 prefix = '!/'
 owo = ['owo', 'OwO', 'oWo', 'OWO', 'uwu', 'UwU', 'uWu', 'UWU'] #owo
