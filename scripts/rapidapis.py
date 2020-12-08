cdac7f28b367aecddf42815bdce49625ba9fac30
Ruthenic <mdrakea3@tutanota.com>
2020-12-08 14:58:05 -0500

    Add COVID19 cases by country api to rapidapis.py

--- scripts/rapidapis.py
+++ scripts/rapidapis.py
@@ -4,28 +4,28 @@ import re
 import os
 import random
 http.client._is_legal_header_name = re.compile(rb'[^\s][^:\r\n]*').fullmatch
+try:
+    rapid = os.environ["URBDICID"].replace("\n", "") #try to use heroku config var to get botID
+except: 
+    print("Running on local machine. Using text file...") #if config var not found, utilize botid.txt for bot id
+    try:
+        rapid = open (r'rapidapiid.txt', "r")
+        rapid = rapid.read().replace("\n", "") #read botID back into the var
+    except:
+        try:
+            rapid = open (r'../rapidapiid.txt', "r")
+            rapid = rapid.read().replace("\n", "") #read botID back into the var
+        except:
+            print("Please place valid RapidAPI ID in rapidapiid.txt")
 class urbdic():
     def urban(term, defnum):
         print(defnum)
         if defnum == "":
             defnum = 0
-        try:
-            urbid = os.environ["URBDICID"].replace("\n", "") #try to use heroku config var to get botID
-        except: 
-            print("Running on local machine. Using text file...") #if config var not found, utilize botid.txt for bot id
-            try:
-                urbid = open (r'urbdicid.txt', "r")
-                urbid = urbid.read().replace("\n", "") #read botID back into the var
-            except:
-                try:
-                    urbid = open (r'../urbdicid.txt', "r")
-                    urbid = urbid.read().replace("\n", "") #read botID back into the var
-                except:
-                    print("Please place valid UrbanDic ID in urbdicid.txt")
         conn = http.client.HTTPSConnection("mashape-community-urban-dictionary.p.rapidapi.com")
 
         headers = {
-            'x-rapidapi-key': urbid,
+            'x-rapidapi-key': rapid,
             'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com"
         }
         url = "/define?term=" + term
@@ -40,23 +40,9 @@ class urbdic():
             zerodic = value[defnum]
         return(zerodic)
     def urbrand():
-        try:
-            urbid = os.environ["URBDICID"].replace("\n", "") #try to use heroku config var to get botID
-        except: 
-            print("Running on local machine. Using text file...") #if config var not found, utilize botid.txt for bot id
-            try:
-                urbid = open (r'urbdicid.txt', "r")
-                urbid = urbid.read().replace("\n", "") #read botID back into the var
-            except:
-                try:
-                    urbid = open (r'../urbdicid.txt', "r")
-                    urbid = urbid.read().replace("\n", "") #read botID back into the var
-                except:
-                    print("Please place valid UrbanDic ID in urbdicid.txt")
-        
         conn = http.client.HTTPSConnection("mashape-community-urban-dictionary.p.rapidapi.com")
         headers = {
-        'x-rapidapi-key': urbid,
+        'x-rapidapi-key': rapid,
         'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com"
         }
         os.system('curl https://www.randomlists.com/data/words.json -o out.json')
@@ -72,10 +58,27 @@ class urbdic():
         conn.request("GET", url=url, headers=headers)
         res = conn.getresponse()
         data = res.read()
-        print(data)
         jsoning = data.decode("utf-8")
         fulljson = json.loads(jsoning)
         for key, value in fulljson.items():
             zerodic = value[0]
         zerodic.update({'Word':term})
         return(zerodic)
+class covid19():
+    def covidcountry(code):
+        conn = http.client.HTTPSConnection("covid-19-data.p.rapidapi.com")
+        
+        headers = {
+            'x-rapidapi-key': rapid,
+            'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
+            }
+        
+        conn.request("GET", "/country/code?code=" + code, headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        jsoning = data.decode("utf-8")
+        fulljson = json.loads(jsoning)
+        for key, value in fulljson.items():
+            zerodic = value[0]
+        return(zerodic)
+    
