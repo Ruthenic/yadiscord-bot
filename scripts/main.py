ee284f5e7cc618eef41761cf48f3cb7bfa256968
Ruthenic <mdrakea3@tutanota.com>
2020-12-07 23:57:03 -0500

    Make main.py smaller

--- scripts/main.py
+++ scripts/main.py
@@ -7,19 +7,10 @@ import operator
 import array as arr
 import time
 from datetime import datetime
-#from translate import Translator
 from google_trans_new import google_translator 
 import json
 import urbdic
 
-#import firebase_admin
-#from firebase_admin import credentials,firestore
-
-#revive code if i use a database ever
-#cred = credentials.Certificate('database-key.json')
-#firebase_admin.initialize_app(cred)
-#db = firestore.client()
-#servers_ref = db.collection(u"servers")
 credits = " People who've contributed: \nRuthenic (AD),\ntestersbastarps (onboho),\nGnog3 (Gnog3)"
 help_message = 'YaDiscord Bot\'s commands:\n`!/help` Show the command list.\n`!/credits` Basically credits.\n`!/ping` Ping the bot.\n`!/owo` Print a random OwO/UwU\n`!/say (text)` Make the bot say something.\n`!/range (first-number), (second-number)` Make the bot generate a random number in given range.\n`!/math (math-stuff)` Do simple math\n`!/eval` Evalutate something. Owner only.' #very long string, i know. do i care? no
 prefix = '!/'
@@ -33,15 +24,11 @@ def log_message(user_message, sent_message): #log commands and their replies
 class MyClient(discord.Client):
     async def on_ready(self): 
         print('Logged in as', self.user)
-
     async def on_message(self, message):
-        if str(message.author.id) == '779930937344917534':
-            return #dont reply to the normal version of the bot
-        if str(message.author.id) == '779932298183311372':
-            return #dont reply to the bleeding edge version of the bot
+        if str(message.author.id) == '779930937344917534' or str(message.author.id) == '779932298183311372':
+            return #dont reply to the bot itself
         try:
             if message.content == prefix + 'ping':
-                guild_name = discord.Guild.name
                 sent_message = 'Pinging...'
                 bot_message = await message.channel.send(sent_message)
                 await bot_message.edit(content=f"Pong! {round(((datetime.timestamp(bot_message.created_at)-datetime.timestamp(message.created_at))%1)*1000)}ms " + str(message.author.mention))
@@ -55,47 +42,38 @@ class MyClient(discord.Client):
                 range_string = message.content.replace(prefix + 'range ', "")
                 range1 = range_string.partition(',')[0].replace(',', '')
                 range2 = range_string.partition(',')[2].replace(',', '')
-                print(range1, range2)
-                try:
-                	sent_message = str(random.randrange(int(range1), int(range2)))
-                except ValueError as e:
-                	sent_message = f"range accepts only numbers\nTraceback:{str(e)}"
+                sent_message = str(random.randrange(int(range1), int(range2)))
                 await message.channel.send(sent_message)
                 log_message(message, sent_message)
             if message.content.startswith(prefix + 'math'):
                 math_string = message.content.replace(prefix + 'math ', "")
-                if math_string.startswith('eval('):
-                    await message.channel.send("lmao did you just try to execute an eval statement? bold of you to assume I am dumb enough to use an eval")
-                else:
-                    indiemath = list(math_string) 
-                    whitespace = " "
-                    while (whitespace in indiemath):
-                        indiemath.remove(whitespace)
-                    for math in indiemath:
-                        if math == '+':
-                            indiemath = list(math_string.split('+'))
-                            sent_message = operator.add(int(indiemath[0]), int(indiemath[1]))
-                            break
-                        elif math == '-':
-                            indiemath = list(math_string.split('-'))
-                            sent_message = operator.sub(int(indiemath[0]), int(indiemath[1]))
-                            break
-                        elif math == '*':
-                            indiemath = list(math_string.split('*'))
-                            sent_message = operator.mul(int(indiemath[0]), int(indiemath[1]))
-                            break
-                        elif math == '/':
-                            indiemath = list(math_string.split('/'))
-                            sent_message = operator.truediv(int(indiemath[0]), int(indiemath[1]))
-                            break
-                        else:
-                            sent_message = "not an operator, get punked"
-                    await message.channel.send(sent_message)
-                    log_message(message, sent_message)
+                indiemath = list(math_string) 
+                whitespace = " "
+                while (whitespace in indiemath):
+                    indiemath.remove(whitespace)
+                for math in indiemath:
+                    if math == '+':
+                        indiemath = list(math_string.split('+'))
+                        sent_message = operator.add(int(indiemath[0]), int(indiemath[1]))
+                        break
+                    elif math == '-':
+                        indiemath = list(math_string.split('-'))
+                        sent_message = operator.sub(int(indiemath[0]), int(indiemath[1]))
+                        break
+                    elif math == '*':
+                        indiemath = list(math_string.split('*'))
+                        sent_message = operator.mul(int(indiemath[0]), int(indiemath[1]))
+                        break
+                    elif math == '/':
+                        indiemath = list(math_string.split('/'))
+                        sent_message = operator.truediv(int(indiemath[0]), int(indiemath[1]))
+                        break
+                    else:
+                        sent_message = "not an operator and/or not a simple math statement, get punked"
+                await message.channel.send(sent_message)
+                log_message(message, sent_message)
             if message.content.startswith(prefix + 'eval'):
                 if str(message.author.id) == "762644120589697045":
-                	guild_name = discord.Guild.name
-                	new_sent_message = ""
                 	evalstate = message.content.replace(prefix + 'eval ', "")
                 	sent_message = str(subprocess.check_output(evalstate, shell=True)).replace("b'", "").replace('b"', "").replace("'", "").replace('""', "").split("\\n")
                 	for stdline in sent_message:
@@ -109,10 +87,9 @@ class MyClient(discord.Client):
                     await message.channel.send(sent_message)
                     log_message(message, sent_message)
             if message.content.startswith(prefix + 'owo '):
-                index = random.randrange(0, len(owo))
-                sent_message = owo[index]
+                sent_message = owo[random.randrange(0, len(owo))]
                 if message.content.replace(prefix + 'owo', "") != "":
-                    sent_message = message.content.replace(prefix + 'owo', "") + " " + owo[index]
+                    sent_message = message.content.replace(prefix + 'owo', "") + " " + owo[random.randrange(0, len(owo))]
                 await message.channel.send(sent_message)
                 await message.delete()
                 log_message(message, sent_message)
@@ -133,33 +110,26 @@ class MyClient(discord.Client):
                 count = 0
                 await message.channel.send("WARNING: COMMAND LITERALLY DOESNT DO ANYTHING")
             if message.content.startswith(prefix + 'owoify '):
-                sent_message = message.content.replace(prefix + 'owoify ', "")
-                sent_message = sent_message.replace("r", "w")
-                sent_message = sent_message.replace("u", "w")
+                sent_message = message.content.replace(prefix + 'owoify ', "").replace("r", "w").replace("u", "w")
                 await message.channel.send(sent_message)
                 await message.delete()
                 log_message(message, sent_message)
             if message.content.startswith(prefix + 'translate '):
-                #trans = Translator(to_lang="en", from_lang='autodetect')
                 trans = google_translator()
                 bot_message = await message.channel.send("Translating...")
-                #sent_message = trans.translate(message.content.replace(prefix + 'translate ', ''))
                 sent_message = trans.translate(message.content.replace(prefix + 'translate ', ''), lang_tgt = 'en')
-                sent_message = sent_message.replace("! / translate ", "")
                 await bot_message.edit(content=sent_message)
                 log_message(message, sent_message)
             if message.content.startswith(prefix + 'Übersetzen ') or message.content.startswith(prefix + 'translate-de '):
                 trans = google_translator()
                 bot_message = await message.channel.send("Übersetzen...")
                 sent_message = trans.translate(message.content.replace(prefix + 'translate-de ', ''), lang_tgt = 'de')
-                sent_message = sent_message.replace("!/translate-de ", "")
                 await bot_message.edit(content=sent_message)
                 log_message(message, sent_message)
             if message.content.startswith(prefix + 'translate-ru '):
                 trans = google_translator()
                 bot_message = await message.channel.send("Translating...")
                 sent_message = trans.translate(message.content.replace(prefix + 'translate-ru ', ''), lang_tgt = 'ru')
-                sent_message = sent_message.replace("! / translate ", "")
                 await bot_message.edit(content=sent_message)
                 log_message(message, sent_message)
             if message.content.startswith(prefix + 'urban'):
