ff478bcd329aceb10fd98a32c443959116c1406f
Drake <mdrakea3@tutanota.com>
2021-02-15 13:45:39 -0500

    fine, you win, i will make hell have a space after it, so slightly less annoying

--- scripts/main.py
+++ scripts/main.py
@@ -198,7 +198,7 @@ class MyClient(discord.Client):
                 sent_message = 'Find our github at <https://github.com/Ruthenic/yadiscord-bot>!'
                 await message.channel.send(sent_message)
                 log_message(message, sent_message)
-            if 'hell' in message.content.lower():
+            if 'hell ' in message.content.lower():
                 sent_message = "When I'm lonely, I become hungry...and when I become hungry, I want to choke on that red ████ of yours! ████ your █████ and lick all of your █████ before taking out your █████ and ████ with more teeth until you're screaming ████████ like a fucking baby!" #reference 3, and 3x2=6 and 6+6+6 = 666, show takes place in hell, this is epic
                 await message.channel.send(sent_message)
                 #log_message(message, sent_message)
